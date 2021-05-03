29101b916f4ddbda71a9d1be15199b502d7719a6
David Kerschbaumer <david.kerschbaumer@student.tugraz.at>
2021-05-03 18:57:17 +0200

    create classes sensor and sensor data. start with real time crawling from csv files by sensor and date

--- downloadDatabase.py
+++ downloadDatabase.py
@@ -2,10 +2,25 @@
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
+import json
 
 local_path = "database/"
 base_url = "http://archive.sensor.community/"
 ext = "csv"
+
+sensors_path = "database/sensors.json"
+
+def writeSensorToJson(sensor_id, sensor_type):
+    with open(sensors_path, "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    if sensor_id in data:
+        return
+
+    data[sensor_id] = sensor_type
+    with open(sensors_path, "w", encoding='utf-8') as jsonFile:
+        json.dump(data, jsonFile ,ensure_ascii=False, indent=2)
+
 
 def geURLContent(url):
     return requests.get(url).text
@@ -22,6 +37,22 @@ def fileExists(folder_name, filename):
     else:
         print(folder_name + filename + " creating")
         return False
+
+def downloadSensorIdAndType(folder_url):
+
+    csv_list = getCSVFileNamesInFolder(folder_url, ext)
+    if not csv_list: # empty list
+        print(folder_url + " not exists on server")
+
+    for csv_url in csv_list:
+        filename = csv_url[42:]
+
+        list_of_underlines = [i for i in range(len(filename)) if filename.startswith('_', i)]
+        sensor_type = filename[list_of_underlines[0] + 1: list_of_underlines[1]]
+        sensor_id = filename[list_of_underlines[2] + 1: len(filename) - 4]
+
+        writeSensorToJson(sensor_id, sensor_type)
+
 
 def downloadFolder(folder_url):
 
@@ -50,8 +81,8 @@ def downloadFolder(folder_url):
 
 def download():
 
-    year_range_from = 2012
-    year_range_to = 2013 # excluding
+    year_range_from = 2021
+    year_range_to = 2022 # excluding
 
     month_range_from = 1
     month_range_to = 2 # excluding
@@ -74,7 +105,8 @@ def download():
                     day_str = str(day)
 
                 date = str(year) + '-' + month_str + '-' + day_str
-                downloadFolder(base_url + date + '/')
+                # downloadFolder(base_url + date + '/')
+                # downloadSensorIdAndType(base_url + date + '/')
 
 
 
