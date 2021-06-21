c373474edc7142ac6cea1674ad7f2e0f324311da
David Kerschbaumer <david.kerschbaumer@student.tugraz.at>
2021-06-21 10:03:11 +0200

    changed to scatter mapbox

--- tabs/worldmap.py
+++ tabs/worldmap.py
@@ -18,7 +18,7 @@ import dash_bootstrap_components as dbc
 # connector = aiohttp.TCPConnector(force_close=True)
 
 # sensor_per_city = api.get_sensors(return_sensors=True, num_sensors=1, type=['bme280', 'dht22', 'bmp280'])
-sensor_per_country = api.get_state_sensors(num_cities=1, type=['bme280', 'dht22', 'bmp280'])
+sensor_per_country = api.get_state_sensors(num_cities=1, type=['bmp280', 'dht22', 'bmp280'])
 token = open("keys/mapbox_token").read()
 
 sensors_from_web = None
@@ -74,18 +74,30 @@ def update_map(value_type, refresh=False):
     global sensor_df
     if sensors_from_web is None or refresh is True:
         get_sensors_from_web()
-        sensor_df = pd.json_normalize(sensors_from_web, record_path='sensordatavalues', meta=['timestamp', 'location'])
+        sensor_df = pd.json_normalize(sensors_from_web, record_path='sensordatavalues', meta=['timestamp', 'location', 'sensor'])
         sensor_loc = pd.json_normalize(sensor_df['location'], meta=['latitude', 'longitude'])
+        sensor_type = pd.json_normalize(sensor_df['sensor'], meta=['id', 'sensor_type'])
         sensor_df['latitude'] = sensor_loc['latitude'].astype(float)
         sensor_df['longitude'] = sensor_loc['longitude'].astype(float)
+        sensor_df['sensor_id'] = sensor_type['id'].astype('i')
 
     typed_sensor_df = sensor_df.loc[sensor_df['value_type'] == value_type]
 
-    fig = px.density_mapbox(typed_sensor_df, hover_name='value_type', lat="latitude", lon="longitude", z='value',
+    typed_sensor_df["value"] = pd.to_numeric(typed_sensor_df["value"], downcast="float")
+
+    fig = px.scatter_mapbox(typed_sensor_df, hover_name='value_type', lat="latitude", lon="longitude", color='value',
                             zoom=2,
-                            height=850)
+                            height=850,
+                            custom_data=['sensor_id'],
+                            color_continuous_scale=[
+                                [0.0, "blue"],
+                                [0.1, "green"],
+                                [0.3, "yellow"],
+                                [1, "red"]]
+                            )
     fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token)
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig.update_layout(clickmode='event+select')
 
     fig.update_layout(
         mapbox_style="white-bg",
